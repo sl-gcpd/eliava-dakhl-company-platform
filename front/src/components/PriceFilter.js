576a969f3e8eaf4e09a5eaea8bf3d7d2c02e8764
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-06-06 15:05:55 +0400

    change price filter, add category filter, add search wrapper

--- front/src/components/PriceFilter.js
+++ front/src/components/PriceFilter.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/PriceFilter.css'
 
-export const PriceFilter = ({ handleChange, current }) => {
+export const PriceFilter = ({ handleChange }) => {
   return (
-        <div className="filter">
-          <input className="price-filter" type="range" onInput={ (e) => handleChange(e.target.value) } />
-          <p className="current-price">{ current }</p>
+        <div className="price-box">
+            <h3>filter price</h3>
+            <input className="price-filter" placeholder="from" type="number" onInput={ (e) => handleChange(e.target.value)} />    
+            <input className="price-filter" placeholder="to" type="number" onInput={ (e) => handleChange(e.target.value)} />    
         </div>
     )
 }
--- front/src/components/SearchBar.js
+++ front/src/components/SearchBar.js
@@ -4,18 +4,7 @@ import '../styles/SearchBar.css'
 
 
 export const SearchBar = () => {
-    const [isButtonVisible, setIsButtonVisible] = useState(false)
-
     return (
-      <div className="search-bar">
-      
-        <input className="search-input" type="text" placeholder="Search" onClick={() => setIsButtonVisible(true)}/>
-      
-        <button style={{
-            visibility: isButtonVisible ? "visible" : "hidden",
-            opacity: isButtonVisible ? 1 : 0
-        }}>GO</button>  
-      
-      </div>
+        <input className="search-input" type="text" placeholder="Search" onClick={() => {}}/>
     )
 }
