d967f0e9dbfcc33beaad3ecd6a651776639e4d58
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-06-06 13:23:37 +0400

    add search bar component

---
+++ front/src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import '../styles/SearchBar.css'
+
+
+export const SearchBar = () => {
+    const [isButtonVisible, setIsButtonVisible] = useState(false)
+
+    return (
+      <div className="search-bar">
+      
+        <input type="text" placeholder="Search" onClick={() => setIsButtonVisible(true)}/>
+      
+        <button style={{
+            visibility: isButtonVisible ? "visible" : "hidden",
+            opacity: isButtonVisible ? 1 : 0
+        }}>GO</button>  
+      
+      </div>
+    )
+}
