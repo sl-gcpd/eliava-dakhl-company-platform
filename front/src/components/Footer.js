de8e34f19b7463bcff4fd02fb74a4979ce6c76fd
Kristine Gudavadze <99663775+FyoDor88@users.noreply.github.com>
2022-06-09 05:26:16 +0400

    Update Footer.js

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
+import "../styles/Footer.css";
 
-export const Footer = () => {
+function Footer() {
   return (
-    <div></div>
-  )
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>ELIAVA DAKHL</h4>
+            <h1 className="list-unstyled">
+              <li>533-32-43-54</li>
+              <li>Tbilisi, Georgia</li>
+              <li>Baratashvili str</li>
+            </h1>
+          </div>
+          
+          <div className="col">
+            <h4>Stuff</h4>
+            <ui className="list-unstyled">
+              <li>about</li>
+              <li>contact</li>
+              <li>help</li>
+            </ui>
+          </div>
+          
+          <div className="col">
+            <h4>WELL ANOTHER COLUMN</h4>
+            <ui className="list-unstyled">
+              <li>about</li>
+              <li>contact</li>
+              <li>help</li>
+            </ui>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} ELIAVA DAKHL | All rights reserved |
+            Terms Of Service | Privacy
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Footer;
