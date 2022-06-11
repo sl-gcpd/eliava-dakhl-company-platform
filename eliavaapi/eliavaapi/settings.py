8f48070ccce5716e91cd481794edfd02b51b482b
hattori <mchedlishvili.nugzari@kiu.edu.ge>
2022-06-11 14:28:54 +0400

    add product model

--- eliavaapi/eliavaapi/settings.py
+++ eliavaapi/eliavaapi/settings.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -120,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- eliavaapi/product/models.py
+++ eliavaapi/product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(null=False,max_length=255,unique=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inStock = models.IntegerField()
+    minQuantity = models.IntegerField()
+    description =  models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null = True)
+
