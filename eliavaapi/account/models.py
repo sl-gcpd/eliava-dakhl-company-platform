2fe4c4b042406458e1a21402cd8e2531859932e8
samKHa92 <samkha@gfmstudio.com>
2022-06-26 13:10:43 +0400

    Fixed CustomUser model bug

--- eliavaapi/account/models.py
+++ eliavaapi/account/models.py
@@ -59,6 +59,11 @@ class CustomUser(AbstractBaseUser):
     def __str__(self):
         return self.firstName+self.lastName
 
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
 
     @property
     def is_staff(self):
