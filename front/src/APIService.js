f60ceef5d804615cd76e3398f4b53d84c159aac2
samKHa92 <samkha@gfmstudio.com>
2022-06-28 23:17:56 +0400

    Implemented APIService methods for authorization endpoints

--- front/src/APIService.js
+++ front/src/APIService.js
@@ -15,4 +15,52 @@ export default class APIService {
             },
         }).then((resp) => resp.json());
     }
+
+    static Login(body) {
+        return fetch("http://127.0.0.1:8000/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        }).then((resp) => resp.json());
+    }
+
+    static Register(body) {
+        return fetch("http://127.0.0.1:8000/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        }).then((resp) => resp.json());
+    }
+
+    static Logout() {
+        return fetch("http://127.0.0.1:8000/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((resp) => resp.json());
+    }
+
+    static ChangePassword(body) {
+        return fetch("http://127.0.0.1:8000/auth/change-password", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        }).then((resp) => resp.json());
+    }
+
+    static UserDetails(id) {
+        return fetch("http://127.0.0.1:8000/auth/user/${id}", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((resp) => resp.json());
+    }
 }
