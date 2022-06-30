94e1e29bafbb29431467df24d0d4942e35c1a363
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-06-30 20:14:00 +0400

    modify authorize box

--- front/src/pages/AuthorizePage.js
+++ front/src/pages/AuthorizePage.js
@@ -1,31 +1,32 @@
 import React from 'react'
-import { Button } from '../components/Button'
-import { TextField } from '../components/TextField'
-import { useState } from 'react'
+import {TextField} from '../components/TextField'
+import {useState} from 'react'
 import '../styles/AuthorizePage.css'
 
- const AuthorizePage = ({isSigningUp}) => {
-  const [ isSignUp, setIsSignUp ] = useState(isSigningUp)
+const AuthorizePage = ({isSigningUp}) => {
+    const [isSignUp, setIsSignUp] = useState(isSigningUp)
 
-  return (
-    <div className="authorize-box">
-      <form>
-      
-      <TextField type="text" placeholder="name" isVisible = { isSignUp } />
-      <TextField type="text" placeholder="email"/>
-      <TextField type="password" placeholder="pass"/>
-      <TextField type="password" placeholder="repeat pass" isVisible = { isSignUp } />
-    
-      <Button text={isSignUp ? "sign up" : "sign in"} onClick={(e) => { 
-        // temporary action
-        e.preventDefault(); 
-        console.log(e) } 
-        } />
-        
-      <p className="register-hint">or <a onClick={() => setIsSignUp(!isSignUp)}>{isSignUp ? "sign in" : "sign up"}</a></p>
+    return (
+        <div className="authorize-box">
+            <form>
 
-      </form>
-    </div>
-  )
+                <TextField type="text" placeholder="name" isVisible={isSignUp}/>
+                <TextField type="text" placeholder="email"/>
+                <TextField type="password" placeholder="pass"/>
+                <TextField type="password" placeholder="repeat pass" isVisible={isSignUp}/>
+
+                <button className="authorize-button" onClick={(e) => {
+                    // temporary action
+                    e.preventDefault();
+                    console.log(e)
+                }
+                }>{isSignUp ? "sign up" : "sign in"}</button>
+
+                <p className="register-hint">or <a
+                    onClick={() => setIsSignUp(!isSignUp)}>{isSignUp ? "sign in" : "sign up"}</a></p>
+
+            </form>
+        </div>
+    )
 }
 export default AuthorizePage;
