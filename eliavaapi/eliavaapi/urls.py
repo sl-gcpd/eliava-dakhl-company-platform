4c4794e46f4049674515abd294f56d66c6df5ecc
samKHa92 <samkha@gfmstudio.com>
2022-06-30 21:12:55 +0400

    Added cart model and endpoints in back

--- eliavaapi/eliavaapi/urls.py
+++ eliavaapi/eliavaapi/urls.py
@@ -24,7 +24,8 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('product.urls')),
-    path('auth/',include('account.urls'))
+    path('auth/',include('account.urls')),
+    path('cart/',include('cart.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
