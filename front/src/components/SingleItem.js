f475d75b6dcfc1f28da325c01d89b83d8bb2d355
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-06-30 19:50:15 +0400

    modify single item

--- front/src/components/SingleItem.js
+++ front/src/components/SingleItem.js
@@ -1,7 +1,24 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "../styles/SingleItem.css";
+import APIService from "../APIService";
 
-export const SingleItem = ({name, img, categories, price, description}) => {
+export const SingleItem = ({id}) => {
+
+    const [img, setImg] = useState("")
+    const [name, setName] = useState("")
+    const [price, setPrice] = useState(0)
+    const [categories, setCategories] = useState("")
+    const [description, setDescription] = useState("")
+
+    useEffect(() => {
+        APIService.GetProduct(id).then(resp => {
+            setImg(resp.image)
+            setName(resp.name)
+            setPrice(resp.price)
+            setCategories(resp.type)
+            setDescription(resp.description)
+        })
+    }, [])
 
     return (
         <div className="single-item-box">
@@ -15,16 +32,12 @@ export const SingleItem = ({name, img, categories, price, description}) => {
                     </div>
                     <div className="single-buttons">
                         <button id="single-price">${price}</button>
-                        <button id="single-cart">cart</button>
+                        <button id="single-cart">Add to cart</button>
                     </div>
                 </div>
             </div>
             <ul>
-                {categories &&
-                    categories.map((e) =>
-                        <li>{e}</li>
-                    )
-                }
+                        <li>{categories}</li>
             </ul>
             <p className="single-description">{description}</p>
         </div>
