c231e8cea8f09b589e70b537223692ba44711e3c
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-06-30 17:33:49 +0400

    add GOOD menu

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,64 @@
 import './App.css';
 import {HomePage} from "./pages/HomePage";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+import APIService from "./APIService";
+import {useEffect, useState} from "react";
+import {Search} from "./components/Search";
 
 const App = () => {
+
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        APIService.GetAllProducts().then((resp) => {
+            setProducts(resp);
+        })
+    }, [])
+
+    const singleBtnClicked = (searchInput, priceFilters, categoryFilters) => {
+        const types = new Map(Object.entries(categoryFilters));
+        const dict = {
+            "Common": "CM",
+            "Finishing": "FS",
+            "Box": "BX",
+            "Roofing": "RF",
+            "Masonry": "MS",
+            "Double Headed": "DH",
+            "Drywall": "DW",
+            "Annual Ring Shank": "RS",
+            "Carpet": "CP",
+            "Staple": "ST"
+        }
+        let type = '';
+        types.forEach((value, key) => {
+            if (value === true) {
+                type += dict[key];
+                type += "_";
+            }
+        });
+        type.slice(0, -1);
+        let qp = ``;
+        if (searchInput !== "")
+            qp += `kword=${searchInput}&`;
+        if (priceFilters[0] !== "")
+            qp += `priceFrom=${priceFilters[0]}&`
+        if (priceFilters[1] !== "")
+            qp += `priceTo=${priceFilters[1]}`
+        if (type !== '')
+            qp += `&type=${type}`;
+        APIService.GetFilteredProducts(qp).then((resp) => {
+            setProducts(resp);
+        })
+    }
+
     return (
         <div className="container">
-            <HomePage/>
+            <div className="menu-search">
+                <Header/>
+                <Search searchBtnClicked={singleBtnClicked}
+                        categories={["Common", "Finishing", "Box", "Roofing", "Masonry", "Double Headed", "Drywal", "Annual Ring Shank", "Carpet", "Staple"]}/>
+            </div>
+            <HomePage products={products} setProducts={setProducts}/>
             <Footer/>
         </div>
     );
