7188a09bddb3334e1ef9ea6c7bfa83a4c313efa6
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-06-30 21:01:47 +0400

    add account page

--- front/src/pages/MyAccountPage.js
+++ front/src/pages/MyAccountPage.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Image from '../img/download.jpeg'
+import '../styles/MyAccountPage.css'
 
 const MyAccountPage = () => {
-  return (
-    <div>MyAccount</div>
-  )
+    return (
+        <div className="account-box">
+            <div className="account-top">
+                <img alt="" src={Image}/>
+                <h1>Ramaz</h1>
+            </div>
+            <div className="account-info">
+                <div className="account-info-item">
+                    <h2>Email</h2>
+                    <p>numerical.prostitute@kiu.edu.ge</p>
+                </div>
+
+                <div className="account-info-item">
+                    <h2>Date of Birth</h2>
+                    <p>32 February, 1666</p>
+                </div>
+
+                <div className="account-info-item">
+                    <h2>Account Type</h2>
+                    <p>Dekan</p>
+                </div>
+
+
+            </div>
+        </div>
+    )
 }
 export default MyAccountPage;
